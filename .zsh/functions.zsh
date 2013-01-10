94bdcbd6866e74e56a0c31722dbb47a18c44b086
Daichi TOMA <amothic@gmail.com>
2013-01-11 02:50:20 +0900

    add flvget

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,6 +1,11 @@
-function gd() {
+function gd () {
     dirs -v
     echo -n "select number: "
     read newdir
     pushd +"$newdir"
 }
+
+function flvget () {
+    cp `ls -1 /var/folders/**/FlashTmp* | sed -e ${2:-1}\!d` ~/Movies/$1.mp4
+}
+
