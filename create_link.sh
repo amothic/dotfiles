954afc243c1cb6fe1696f67b06b1d980a8177a82
Daichi TOMA <amothic@gmail.com>
2016-02-22 16:00:12 +0900

    Refactoring gitconfig

--- create_link.sh
+++ create_link.sh
@@ -2,12 +2,11 @@
 cd $(dirname $0)
 for dotfile in .?*
 do
-	if [ $dotfile != '..' ] && [[ $dotfile != \.git* ]]
-	then
-		ln -Fis "$PWD/$dotfile" $HOME
-	fi
+    if [ $dotfile != '..' ] && [[ $dotfile != \.git* ]]; then
+        ln -Fis "$PWD/$dotfile" $HOME
+    fi
 done
 
 # exception
-
 ln -Fis "$PWD/.gitconfig" $HOME
+ln -Fis "$PWD/.gitignore.global" $HOME
