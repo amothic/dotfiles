119141586b10c9a72fb91d7f45a665d589f85dce
Daichi TOMA <amothic@gmail.com>
2016-02-23 14:37:58 +0900

    do smart

--- .zsh/external.zsh
+++ .zsh/external.zsh
@@ -1,7 +1,9 @@
-if [ -d $HOME/.rbenv ]; then
-    source ~/.rbenv/completions/rbenv.zsh
+MY_RBENV_COMPLETION=`brew --prefix rbenv`/completions/rbenv.zsh
+if [ -x `which brew` ] && [ -r $MY_RBENV_COMPLETION ]; then
+    source $MY_RBENV_COMPLETION
 fi
 
-if [ -x `which brew` ] && [ -r `brew --prefix`/etc/profile.d/z.sh ]; then
-    . `brew --prefix`/etc/profile.d/z.sh
+MY_Z=`brew --prefix`/etc/profile.d/z.sh
+if [ -x `which brew` ] && [ -r $MY_Z ]; then
+    . $MY_Z
 fi
