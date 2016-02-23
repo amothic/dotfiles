5bc6f9bdd11d61c15d5564e31bcb59ac58882f39
Daichi TOMA <amothic@gmail.com>
2016-02-23 13:28:41 +0900

    Fix bug

--- .zsh/external.zsh
+++ .zsh/external.zsh
@@ -6,6 +6,6 @@ if [ -d $HOME/.rbenv ]; then
     source ~/.rbenv/completions/rbenv.zsh
 fi
 
-if [ -x brew ] && [ -r `brew --prefix`/etc/profile.d/z.sh ]; then
+if [ -x `which brew` ] && [ -r `brew --prefix`/etc/profile.d/z.sh ]; then
     . `brew --prefix`/etc/profile.d/z.sh
 fi
