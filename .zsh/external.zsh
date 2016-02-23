13588fd2ccd4777733e76192bc0cd52696993c9c
Daichi TOMA <amothic@gmail.com>
2016-02-23 14:10:24 +0900

    Remove useless settings

--- .zsh/external.zsh
+++ .zsh/external.zsh
@@ -2,10 +2,6 @@ if [ -d $HOME/.rbenv ]; then
     source ~/.rbenv/completions/rbenv.zsh
 fi
 
-if [ -d $HOME/.rbenv ]; then
-    source ~/.rbenv/completions/rbenv.zsh
-fi
-
 if [ -x `which brew` ] && [ -r `brew --prefix`/etc/profile.d/z.sh ]; then
     . `brew --prefix`/etc/profile.d/z.sh
 fi
