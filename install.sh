10dbfa4086bedd0562304c22e0849dcc0f184432
Daichi TOMA <amothic@gmail.com>
2017-04-20 17:56:20 +0900

    Fix install scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 cd $(dirname $0)
 
+echo "Change Shell to Zsh"
+chsh -s /bin/zsh
+
+echo "Create dotfile links"
 $PWD/create_link.sh
+
+echo "Install vim plugin"
 $PWD/vim.sh
 
+echo "Install Homebrew and Install Apps"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap Homebrew/bundle
 brew bundle
