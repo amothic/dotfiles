8efee320c8d98984189196c8d92d4e30dd318e06
Daichi TOMA <amothic@gmail.com>
2021-01-23 00:27:23 +0900

    Add homebrew settings

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -14,6 +14,14 @@ sudo_path=(
 /usr/local/sbin(N-/)
 )
 
+# Homebrew
+export HOMEBREW_PREFIX="/opt/homebrew";
+export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+export HOMEBREW_REPOSITORY="/opt/homebrew";
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
+export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+
 MY_RBENV="$HOME/.rbenv"
 if [ -d $MY_RBENV ]; then
     path=(
