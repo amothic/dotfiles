426546adbda32bfe7ca7afc3f2e6cb9e488aa02a
Daichi TOMA <amothic@gmail.com>
2021-04-20 18:50:47 +0900

    Delete rbenv and plenv settings

--- .zsh/external.zsh
+++ .zsh/external.zsh
@@ -1,13 +1,3 @@
-MY_RBENV_COMPLETION=`brew --prefix rbenv`/completions/rbenv.zsh
-if [ -x `which brew` ] && [ -r $MY_RBENV_COMPLETION ]; then
-    source $MY_RBENV_COMPLETION
-fi
-
-MY_PLENV_COMPLETION=`brew --prefix plenv`/completions/plenv.zsh
-if [ -x `which brew` ] && [ -r $MY_PLENV_COMPLETION ]; then
-    source $MY_PLENV_COMPLETION
-fi
-
 MY_Z=`brew --prefix`/etc/profile.d/z.sh
 if [ -x `which brew` ] && [ -r $MY_Z ]; then
     source $MY_Z
