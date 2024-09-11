4a47c38c66ff17de885ea2699324082d31c17144
Daichi TOMA <amothic@gmail.com>
2024-09-11 20:33:39 +0900

    use eza

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,12 +1,4 @@
+alias ls='eza --icons'
 alias cp='cp -i'
 alias mv='mv -i'
 alias tmux='tmux -u'
-
-case "${OSTYPE}" in
-    freebsd*|darwin*)
-        alias ls='ls -G -w'
-        ;;
-    linux*)
-        alias ls='ls --color'
-        ;;
-esac
