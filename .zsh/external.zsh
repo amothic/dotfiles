9e8e72994d01966c4f14b5eafb2ecb3bd0b44627
Daichi TOMA <amothic@gmail.com>
2024-09-11 20:19:33 +0900

    fix comment

--- .zsh/external.zsh
+++ .zsh/external.zsh
@@ -1,17 +1,15 @@
+# z
 MY_Z=`brew --prefix`/etc/profile.d/z.sh
 if [ -x `which brew` ] && [ -r $MY_Z ]; then
     source $MY_Z
 fi
 
+# google-cloud-sdk
 export CLOUDSDK_PYTHON=$(which python3)
-
-# Update PATH for the Google Cloud SDK.
 MY_GCLOUD=~/google-cloud-sdk/path.zsh.inc
 if [ -f $MY_GCLOUD ]; then
     source $MY_GCLOUD
 fi
-
-# Enable shell command completion for gcloud.
 MY_GCLOUD_COMPLETION=~/google-cloud-sdk/completion.zsh.inc
 if [ -f $MY_GCLOUD_COMPLETION ]; then
     source $MY_GCLOUD_COMPLETION
