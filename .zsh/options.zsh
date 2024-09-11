68981164c4fd00d900b85c800c451e483942bcbc
Daichi TOMA <amothic@gmail.com>
2024-09-11 20:47:02 +0900

    delete useless settings

--- .zsh/options.zsh
+++ .zsh/options.zsh
@@ -16,21 +16,3 @@ setopt nolistbeep
 setopt ignore_eof
 setopt no_flow_control
 setopt long_list_jobs
-
-zstyle ':completion:*' format '%B%d%b'
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*:default' menu select=2
-zstyle ':completion:*' list-colors 'di=34' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z} r:|[._-]=*'
-zstyle ':completion:*' completer \
-        _oldlist _complete _match _history _ignored _approximate _prefix
-zstyle ':completion:*' use-cache yes
-zstyle ':completion:*' verbose yes
-zstyle ':completion:sudo:*' environ PATH="$SUDO_PATH:$PATH"
-
-# ls color
-export LSCOLORS=exfxcxdxbxegedabagacad
-export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
-
-# grep options
-export GREP_OPTIONS="--color=auto"
