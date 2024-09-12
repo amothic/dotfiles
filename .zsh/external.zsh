fed49a998b22f262da02c5e14cc2a613c411069a
Daichi TOMA <amothic@gmail.com>
2024-09-12 12:38:37 +0900

    use zoxide

--- .zsh/external.zsh
+++ .zsh/external.zsh
@@ -1,18 +1,18 @@
 # z
-MY_Z=`brew --prefix`/etc/profile.d/z.sh
-if [ -x `which brew` ] && [ -r $MY_Z ]; then
-    source $MY_Z
+if type zoxide &> /dev/null; then
+  eval "$(zoxide init zsh)"
 fi
+
 
 # google-cloud-sdk
 export CLOUDSDK_PYTHON=$(which python3)
 MY_GCLOUD=~/google-cloud-sdk/path.zsh.inc
 if [ -f $MY_GCLOUD ]; then
-    source $MY_GCLOUD
+  source $MY_GCLOUD
 fi
 MY_GCLOUD_COMPLETION=~/google-cloud-sdk/completion.zsh.inc
 if [ -f $MY_GCLOUD_COMPLETION ]; then
-    source $MY_GCLOUD_COMPLETION
+  source $MY_GCLOUD_COMPLETION
 fi
 
 # fzf
