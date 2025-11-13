0f1a81c880afaa07c368c55a1dfad3a3bd73c52b
Daichi TOMA <amothic@gmail.com>
2025-11-13 18:05:21 +0900

    Remove redundant PATH configurations managed by asdf
    PoetryとJavaは使用していないため削除。GoとDartはasdfのshimsで管理されているため、個別のPATH設定は不要。

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -27,39 +27,3 @@ if [ -d $MY_ASDF ]; then
     $path
     )
 fi
-
-MY_GO="$HOME/go"
-if [ -d $MY_GO ]; then
-    export GOPATH=$MY_GO
-    path=(
-    $MY_GO/bin(N-/)
-    $path
-    )
-fi
-
-# Dart
-MY_DART="$HOME/.pub-cache/bin"
-if [ -d $MY_DART ]; then
-    path=(
-    $MY_DART(N-/)
-    $path
-    )
-fi
-
-# Poetry
-MY_POETRY="$HOME/.local/bin"
-if [ -d $MY_POETRY ]; then
-    path=(
-    $MY_POETRY(N-/)
-    $path
-    )
-fi
-
-# Java
-MY_JAVA="/opt/homebrew/opt/openjdk/bin"
-if [ -d $MY_JAVA ]; then
-    path=(
-    $MY_JAVA(N-/)
-    $path
-    )
-fi
